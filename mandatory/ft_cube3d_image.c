7fa44297508c36d3b2b50456454cc16e2d7d9534
Geunsu Yoon <geuyoon@cx1r2s1.42seoul.kr>
2025-03-29 16:00:28 +0900

    feat: Make logic to add all wall texture

--- mandatory/ft_cube3d_image.c
+++ mandatory/ft_cube3d_image.c
@@ -6,48 +6,72 @@
 /*   By: geuyoon <geuyoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 01:54:26 by geuyoon           #+#    #+#             */
-/*   Updated: 2025/03/20 02:33:53 by geuyoon          ###   ########.fr       */
+/*   Updated: 2025/03/29 15:58:37 by geuyoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_cube3d.h"
 
-t_image	*parse_image(t_data *data, int map_fd, char *ident);
+void	parse_image(t_data *data, int map_fd);
+int		image_setter(t_data *data, char *ident, char *path);
 t_image	*init_image(t_data *data, char *path);
 
-t_image	*parse_image(t_data *data, int map_fd, char *ident)
+void	parse_image(t_data *data, int map_fd)
 {
-	t_image	*new_image;
 	char	*tmp_line;
 	char	**tmp_td_line;
-
+	
 	tmp_line = get_next_line(map_fd);
 	while (tmp_line && !ft_strlen(tmp_line))
 		tmp_line = get_next_line(map_fd);
-	if (!tmp_line)
+		if (!tmp_line)
 		exit_err(data, 0, 0);
-	tmp_td_line = ft_split(tmp_line, ' ');
-	free(tmp_line);
-	if (!tmp_td_line)
+		tmp_td_line = ft_split(tmp_line, ' ');
+		free(tmp_line);
+		if (!tmp_td_line)
 		exit_err(data, 0, 0);
-	if (ft_strtdlen(tmp_td_line) != 2)
+		if (ft_strtdlen(tmp_td_line) != 2)
 		exit_err(data, 0, 0);
-	if (ft_strncmp(ident, tmp_td_line[0], ft_strlen(tmp_td_line[0])))
-		exit_err(data, "unexpected map file information", 0);
-	new_image = init_image(data, tmp_td_line[1]);
+		if (image_setter(data, tmp_td_line[0], tmp_td_line[1]))
+		{
+			free_td_str(tmp_td_line, 2);
+		exit_err(data, "unexpected image infomation", 1);
+	}
 	free_td_str(tmp_td_line, 2);
-	return (new_image);
+}
+
+int	image_setter(t_data *data, char *ident, char *path)
+{
+	if (!ft_strcmp(ident, MNORTH))
+		data->no = init_image(data, path);
+	else if (!ft_strcmp(ident, MSOUTH))
+		data->so = init_image(data, path);
+	else if (!ft_strcmp(ident, MWEST))
+		data->we = init_image(data, path);
+	else if (!ft_strcmp(ident, MEAST))
+		data->ea = init_image(data, path);
+	else
+		return (1);
+	return (0);
 }
 
 t_image	*init_image(t_data *data, char *path)
 {
 	t_image	*new_image;
+	int		image_fd;
 
+	image_fd = open(path, O_RDONLY);
+	if (image_fd == -1)
+		exit_err(data, 0, 0);
+	close(image_fd);
 	new_image = ft_calloc(1, sizeof(t_image));
 	if (!new_image)
 		exit_err(data, 0, 0);
-	new_image->image_path = ft_strdup(path);
-	if (!new_image->image_path)
+	new_image->img_ptr = mlx_xpm_file_to_image(data->mlx_ctl->mlx, path, &(new_image->width), &(new_image->height));
+	if (new_image->img_ptr == 0)
+	{
+		free(new_image);
 		exit_err(data, 0, 0);
+	}
 	return (new_image);
 }
