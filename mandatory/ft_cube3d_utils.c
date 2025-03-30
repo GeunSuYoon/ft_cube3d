1940d1ff8129e40cc1f82b9535fe1588a649f4fd
Geunsu Yoon <geuyoon@cx1r2s1.42seoul.kr>
2025-03-30 16:12:50 +0900

    refactor: Split allocation codes and add my strcmp func

--- mandatory/ft_cube3d_utils.c
+++ mandatory/ft_cube3d_utils.c
@@ -6,13 +6,12 @@
 /*   By: geuyoon <geuyoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 00:34:34 by geuyoon           #+#    #+#             */
-/*   Updated: 2025/03/29 16:32:11 by geuyoon          ###   ########.fr       */
+/*   Updated: 2025/03/29 16:37:35 by geuyoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_cube3d.h"
 
-void	free_td_str(char **str, size_t str_height);
 size_t	ft_strtdlen(char **tdstr);
 int		ft_strcmp(char *s1, char *s2);
 
@@ -26,20 +25,6 @@ size_t	ft_strtdlen(char **tdstr)
 	return (cnt);
 }
 
-void	free_td_str(char **str, size_t str_height)
-{
-	size_t	height_cnt;
-
-	height_cnt = 0;
-	while (height_cnt < str_height)
-	{
-		free(str[height_cnt]);
-		str[height_cnt] = 0;
-		height_cnt++;
-	}
-	free(str);
-	str = 0;
-}
 
 int	ft_strcmp(char *s1, char *s2)
 {
