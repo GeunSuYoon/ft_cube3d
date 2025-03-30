b68321014c1b6960ebb80f716994550582ef5ae6
Geunsu Yoon <geuyoon@cx1r2s1.42seoul.kr>
2025-03-30 16:11:19 +0900

    feat: Add map read func and map error check func

--- mandatory/ft_cube3d_map.c
+++ mandatory/ft_cube3d_map.c
@@ -6,7 +6,7 @@
 /*   By: geuyoon <geuyoon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 00:58:01 by geuyoon           #+#    #+#             */
-/*   Updated: 2025/03/19 05:25:03 by geuyoon          ###   ########.fr       */
+/*   Updated: 2025/03/30 16:06:11 by geuyoon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,24 @@
 
 t_map	*init_map(t_data *data, int map_fd);
 char	**read_map(t_data *data, t_map *map, int map_fd);
+char	**realloc_map(t_data *data, t_map *map, char **map_data, char *tmp_map);
+void	map_checker(t_data *data, t_map *map);
 
-t_map	*init_map(t_data *data,  int map_fd)
+t_map	*init_map(t_data *data, int map_fd)
 {
 	t_map	*new_map;
 
 	new_map = ft_calloc(1, sizeof(t_map));
 	if (!new_map)
 		exit_err(data, 0, 0);
+	data->map = new_map;
+	new_map->p_pos = ft_calloc(1, sizeof(t_pos));
+	if (!new_map->p_pos)
+		exit_err(data, 0, 0);
+	new_map->map_height = 0;
+	new_map->map_width = 0;
+	new_map->map_data = read_map(data, new_map, map_fd);
+	map_checker(data, new_map);
 }
 
 char	**read_map(t_data *data, t_map *map, int map_fd)
@@ -32,17 +42,63 @@ char	**read_map(t_data *data, t_map *map, int map_fd)
 	total_map = ft_calloc(1, sizeof(char *));
 	if (total_map == 0)
 		exit_err(data, 0, 0);
-	while (1)
+	tmp_map = get_next_line(map_fd);
+	while (!ft_strlen(tmp_map))
 	{
+		free(tmp_map);
 		tmp_map = get_next_line(map_fd);
+	}
+	while (tmp_map && ft_strlen(tmp_map))
+	{
 		if (tmp_map == 0)
-		{
-			map_close_check(data, map, tmp_map, map_fd);
-			break ;
-		}
-		map_size(data, map, tmp_map);
-		total_map = realloc_map(map, total_map, tmp_map);
+		break ;
+		if (map->map_width < ft_strlen(tmp_map))
+		map->map_width = ft_strlen(tmp_map);
+		total_map = realloc_map(data, map, total_map, tmp_map);
+		tmp_map = get_next_line(map_fd);
 	}
 	close(map_fd);
 	return (total_map);
+}
+
+char	**realloc_map(t_data *data, t_map *map, char **map_data, char *tmp_map)
+{
+	char	**new_map_data;
+	size_t	height_cnt;
+
+	if (tmp_map[0] == '\n')
+		return (map_data);
+	map->map_height++;
+	new_map_data = ft_calloc(map->map_height, sizeof(char *));
+	if (!new_map_data)
+		exit_err(data, 0, 0);
+	height_cnt = 0;
+	while (height_cnt < map->map_height - 1)
+	{
+		new_map_data[height_cnt] = ft_strdup(map_data[height_cnt]);
+		if (!new_map_data[height_cnt])
+		{
+			free_td_str(new_map_data, height_cnt - 1);
+			free_td_str(map_data, map->map_height - 1);
+			exit_err(data, 0, 0);
+		}
+		height_cnt++;
+	}
+	new_map_data[map->map_height - 1] = tmp_map;
+	free_td_str(map_data, height_cnt - 1);
+	map_data = 0;
+	return (new_map_data);
+}
+
+void	map_checker(t_data *data, t_map *map)
+{
+	int	**round_checker;
+
+	round_checker = init_round_checker(data, map);
+	wall_copier(data, map, round_checker);
+	if (!map->p_pos->x && !map->p_pos->y)
+		map_check_exit(data, round_checker, "no player info", 1);
+	else if (!map->p_pos->x || !map->p_pos->y)
+		map_check_exit(data, round_checker, "unexpected player pos", 1);
+	map_dp(data, round_checker, (int)map->p_pos->x, (int)map->p_pos->y);
 }
---
+++ mandatory/ft_cube3d_map_check.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cube3d_map_check.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: geuyoon <geuyoon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/30 15:12:31 by geuyoon           #+#    #+#             */
+/*   Updated: 2025/03/30 15:39:03 by geuyoon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_cube3d.h"
+
+void	wall_copier(t_data *data, t_map *map, int **round_checker);
+void	map_dp(t_data *data, int **round_checker, int x, int y);
+int		**init_round_checker(t_data *data, t_map *map);
+int		map_ele_checker(char ele);
+void	map_check_exit(t_data *data, int **round_checker, char *str, int errsig);
+
+void	wall_copier(t_data *data, t_map *map, int **round_checker)
+{
+	size_t	h_cnt;
+	size_t	w_cnt;
+	int		ele_check;
+
+	h_cnt = 0;
+	while (h_cnt < map->map_height)
+	{
+		w_cnt = 0;
+		while (map->map_data[h_cnt][w_cnt])
+		{
+			ele_check = map_ele_checker(map->map_data[h_cnt][w_cnt]);
+			if (!ele_check)
+				map_check_exit(data, round_checker, "unexpected map info", 1);
+			else if (ele_check == 1)
+				round_checker[h_cnt][w_cnt] = 1;
+			else if (ele_check == 4)
+			{
+				map->p_pos->x = w_cnt;
+				map->p_pos->y = h_cnt;
+			}
+			w_cnt++;
+		}
+		h_cnt++;
+	}
+}
+
+void	map_dp(t_data *data, int **round_checker, int x, int y)
+{
+	if (round_checker[y][x])
+		return ;
+	if (!x || !y || x == data->map->map_width || y == data->map->map_height)
+		map_check_exit(data, round_checker, "unexpected map shape", 1);
+	round_checker[y][x]++;
+	map_dp(data, round_checker, x + 1, y);
+	map_dp(data, round_checker, x - 1, y);
+	map_dp(data, round_checker, x, y + 1);
+	map_dp(data, round_checker, x, y - 1);
+}
+
+int	**init_round_checker(t_data *data, t_map *map)
+{
+	int	**ret_checker;
+	int	cnt;
+
+	ret_checker = ft_calloc(map->map_height, sizeof(int *));
+	if (!ret_checker)
+		exit_err(data, 0, 0);
+	cnt = 0;
+	while (cnt < map->map_height)
+	{
+		ret_checker[cnt] = ft_calloc(map->map_width, sizeof(int));
+		if (!ret_checker[cnt])
+		{
+			free_td_int(ret_checker, cnt);
+			exit_err(data, 0, 0);
+		}
+		cnt++;
+	}
+	return (ret_checker);
+}
+
+int    map_ele_checker(char ele)
+{
+	if (ele == EMPTY)
+		return (1);
+	if (ele == SPACE)
+		return (2);
+	if (ele == WALL)
+		return (3);
+	if (ele == PNORTH)
+		return (4);
+	if (ele == PSOUTH)
+		return (4);
+	if (ele == PEAST)
+		return (4);
+	if (ele == PWEST)
+		return (4);
+	return (0);
+}
+
+void	map_check_exit(t_data *data, int **round_checker, char *str, int errsig)
+{
+	free_td_int(round_checker, data->map->map_height);
+	exit_err(data, str, errsig);
+}
