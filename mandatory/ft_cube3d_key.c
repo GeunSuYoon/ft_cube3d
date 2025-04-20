609d222d5eff02d2a48ae156521f6a877b856a2d
Geunsu Yoon <geuyoon@cx1r2s1.42seoul.kr>
2025-04-20 12:02:19 +0900

    feat: Add player rotate, move function

--- mandatory/ft_cube3d_key.c
+++ mandatory/ft_cube3d_key.c
@@ -14,8 +14,8 @@
 
 void		key_ctl(t_data *data);
 static int	key_press(int keycode, t_data *data);
-static void	key_angle(int keycode, t_data *data);
-static void	key_move(int keycode, t_data *data);
+static void	key_rot(int keycode, t_player *player);
+static void	key_move(int keycode, t_player *player);
 
 void	key_ctl(t_data *data)
 {
@@ -27,8 +27,58 @@ static int	key_press(int keycode, t_data *data)
 	if (keycode == KESC)
 		close_window(data);
 	if (keycode == KLEFT || keycode == KRIGHT)
-		key_angle(keycode, data);
+		key_rot(keycode, data);
 	else if (keycode == KW || keycode == KA || keycode == KS || keycode == KD)
 		key_move(keycode, data);
 	return (0);
+}
+
+static void	key_rot(int keycode, t_player *player)
+{
+	if (keycode == KLEFT)
+	{
+		if (0 <= player->dir_y)
+			player->dir_x += ROTSPEED;
+		else
+			player->dir_x -= ROTSPEED;
+		if (0 <= player->dir_x)
+			player->dir_y -= ROTSPEED;
+		else
+			player->dir_y += ROTSPEED;
+	}
+	else if (keycode == KRIGHT)
+	{
+		if (0 <= player->dir_y)
+			player->dir_x -= ROTSPEED;
+		else
+			player->dir_x += ROTSPEED;
+		if (0 <= player->dir_x)
+			player->dir_y += ROTSPEED;
+		else
+			player->dir_y -= ROTSPEED;
+	}
+}
+
+static void	key_move(int keycode, t_player *player)
+{
+	if (keycode == KW)
+	{
+		player->dir_x += player->dir_x * MOVESPEED;
+		player->dir_y += player->dir_y * MOVESPEED;
+	}
+	else if (keycode == KA)
+	{
+		player->dir_x -= player->dir_x * MOVESPEED;
+		player->dir_y += player->dir_y * MOVESPEED;
+	}
+	else if (keycode == KS)
+	{
+		player->dir_x -= player->dir_x * MOVESPEED;
+		player->dir_y -= player->dir_y * MOVESPEED;
+	}
+	else if (keycode == KD)
+	{
+		player->dir_x += player->dir_x * MOVESPEED;
+		player->dir_y -= player->dir_y * MOVESPEED;
+	}
 }
