8ef086653724b67683b03d8d3b8f27515049b76c
Geunsu Yoon <geuyoon@cx1r4s4.42seoul.kr>
2025-05-06 16:47:35 +0900

    feat: Change key macro mlx to X11

--- mandatory/mlx_ctl/ft_cube3d_key.c
+++ mandatory/mlx_ctl/ft_cube3d_key.c
@@ -21,15 +21,18 @@ static void	key_move(int keycode, t_player *player, t_map *map, t_rcast *rcast);
 void	key_ctl(t_data *data)
 {
 	mlx_key_hook(data->mlx_ctl->win, &key_press, data);
-	mlx_hook(data->mlx_ctl->mlx, 17, 0, &close_window, data);
+	mlx_hook(data->mlx_ctl->win, 17, 0, &close_window, data);
 }
 
 static int	key_press(int keycode, t_data *data)
 {
-	if (keycode == KESC)
+	if (keycode == XK_Escape)
 		close_window(data);
-	if (keycode == KLEFT || keycode == KRIGHT || \
-		keycode == KW || keycode == KA || keycode == KS || keycode == KD)
+	if (keycode == XK_Left || keycode == XK_Right || \
+		keycode == XK_W || keycode == XK_A || \
+		keycode == XK_S || keycode == XK_D || \
+		keycode == XK_w || keycode == XK_a || \
+		keycode == XK_s || keycode == XK_d)
 		key_player(keycode, data, data->rcast);
 	return (0);
 }
@@ -37,12 +40,13 @@ static int	key_press(int keycode, t_data *data)
 static void	key_player(int keycode, t_data *data, t_rcast *rcast)
 {
 	get_frame_time(rcast);
-	if (keycode == KLEFT || keycode == KRIGHT)
+	if (keycode == XK_Left || keycode == XK_Right)
 		key_rot(keycode, data->player, rcast);
-	else if (keycode == KW || keycode == KA || keycode == KS || keycode == KD)
+	else if (keycode == XK_W || keycode == XK_A || \
+		keycode == XK_S || keycode == XK_D || \
+		keycode == XK_w || keycode == XK_a || \
+		keycode == XK_s || keycode == XK_d)
 		key_move(keycode, data->player, data->map, rcast);
-	if (mlx_clear_window(data->mlx_ctl->mlx, data->mlx_ctl->win) < 0)
-		exit_err(data, 0, 0);
 	cast_window(data);
 }
 
@@ -51,10 +55,10 @@ static void	key_rot(int keycode, t_player *player, t_rcast *rcast)
 	double	rot_speed;
 
 	rot_speed = rcast->fram_time * (double)ROTSPEED;
-	if (keycode == KLEFT)
-		rot_player(player, 1, rot_speed);
-	else if (keycode == KRIGHT)
-		rot_player(player, -1, rot_speed);
+	if (keycode == XK_Left)
+		set_player_dir(player, -1, rot_speed);
+	else if (keycode == XK_Right)
+		set_player_dir(player, 1, rot_speed);
 }
 
 static void	key_move(int keycode, t_player *player, t_map *map, t_rcast *rcast)
@@ -62,16 +66,16 @@ static void	key_move(int keycode, t_player *player, t_map *map, t_rcast *rcast)
 	double	move_speed;
 
 	move_speed = rcast->fram_time * (double)MOVESPEED;
-	if (keycode == KW)
-		move_player(player, map->map_data, player->dir_x * move_speed, \
+	if (keycode == XK_W || keycode == XK_w)
+		set_player_pos(player, map->map_data, player->dir_x * move_speed, \
 			player->dir_y * move_speed);
-	else if (keycode == KA)
-		move_player(player, map->map_data, -player->dir_x * move_speed, \
+	else if (keycode == XK_A || keycode == XK_a)
+		set_player_pos(player, map->map_data, -player->dir_x * move_speed, \
 			player->dir_y * move_speed);
-	else if (keycode == KS)
-		move_player(player, map->map_data, -player->dir_x * move_speed, \
+	else if (keycode == XK_S || keycode == XK_s)
+		set_player_pos(player, map->map_data, -player->dir_x * move_speed, \
 			-player->dir_y * move_speed);
-	else if (keycode == KD)
-		move_player(player, map->map_data, player->dir_x * move_speed, \
+	else if (keycode == XK_D || keycode == XK_d)
+		set_player_pos(player, map->map_data, player->dir_x * move_speed, \
 			-player->dir_y * move_speed);
 }
